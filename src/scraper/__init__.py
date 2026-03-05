50fe0f60d569d3aad9dcaac35c518477e3935334
Kaavya Sri Ramarapu <137331993+kaavyyaashri@users.noreply.github.com>
2026-03-05 07:58:21 -0600

    Update __init__.py

--- src/scraper/__init__.py
+++ src/scraper/__init__.py
@@ -1,4 +1,7 @@
+# from .base_scraper import BaseScraper, Job
+# from .indeed_scraper import IndeedScraper
+# from .glassdoor_scraper import GlassdoorScraper
+# from .naukri_scraper import NaukriScraper
+
 from .base_scraper import BaseScraper, Job
-from .indeed_scraper import IndeedScraper
-from .glassdoor_scraper import GlassdoorScraper
-from .naukri_scraper import NaukriScraper
+from .jsearch_scraper import JSearchScraper
