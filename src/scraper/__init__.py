eec0bb284a78a10e029b779470616e15d9866db3
Kaavya Sri Ramarapu <137331993+kaavyyaashri@users.noreply.github.com>
2026-03-11 22:26:21 -0500

    Update __init__.py

--- src/scraper/__init__.py
+++ src/scraper/__init__.py
@@ -8,3 +8,14 @@
 
 from .base_scraper import BaseScraper, Job
 from .jobspy_scraper import JobSpyScraper
+
+from .jobspy_scraper  import JobSpyScraper
+from .jsearch_scraper import JSearchScraper
+
+def get_scraper(country_config: dict, country_name: str):
+    """Return list of scrapers to run for this country"""
+    scrapers = [
+        JobSpyScraper(country_config, country_name),
+        JSearchScraper(country_config, country_name),
+    ]
+    return scrapers
